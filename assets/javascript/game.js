c464206ff9c951787c044fe8a27ad0dfcb8f9871
Tran <tranti@ga.com>
2018-06-08 12:24:53 -0700

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,3 +33,51 @@ function resetGame() {
     generateGemRandNumber();
     displayRandNumber(randNumberHtml);
 }
+
+let randNumberHtml = $('#random-number');
+        let randomNumber = 0;
+        let buttons = []
+        let sum = 0;
+        let wins = 0;
+        let losses = 0;
+
+        $(document).ready(function () {
+            startGame();
+
+
+            let retrievedValue = 0;
+
+            $('#red-crystal').attr('value', buttons[0]);
+            retrievedValue = $('#red-crystal').attr('value');
+
+            $('#blue-crystal').attr('value', buttons[1]);
+            retrievedValue = $('#blue-crystal').attr('value');
+
+            $('#yellow-crystal').attr('value', buttons[2]);
+            retrievedValue = $('#yellow-crystal').attr('value');
+
+            $('#green-crystal').attr('value', buttons[3]);
+            retrievedValue = $('#green-crystal').attr('value');
+        })
+
+
+        $('button').on('click', function () {
+            sum += parseInt($(this).attr('value'));
+            console.log('The total is: ' + sum);
+            $('#your-guess').html(sum);
+
+
+            if (sum === randomNumber) {
+                wins++;
+                console.log('You win!!!')
+                $('#wins').text('Wins: ' + wins)
+                $('#status').text('Status: Winer')
+                resetGame();
+            } else if (sum > randomNumber) {
+                losses++;
+                console.log('you lose!!!!')
+                $('#losses').text('Losses: ' + losses);
+                $('#status').text('Status: Loser')
+                resetGame();
+            }
+        })
