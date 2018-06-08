cb103c5302192a1d31972016d650f3c69d7fc9a0
Tran <tranti@ga.com>
2018-06-08 12:18:26 -0700

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,35 @@
-
 //this function generates a random number
 function generateRandNumber() {
     randomNumber = Math.floor(Math.random() * 101) + 19;
     console.log('random number: ' + randomNumber);
     return randomNumber;
 }
-
 //this function generates a random number for gem
 function generateGemRandNumber() {
-	for(i = 1; i < 5; i++){
-        gemNumbers[i] = Math.floor(Math.random() * 12) + 1;
-        buttons.push(gemNumbers[i])
-
-        console.log('gem' + i + ' ' + gemNumbers[i]);
-        
-	}
-    
-}
-function generateGemRandNumber1() {
-	
-        gemNumber = Math.floor(Math.random() * 12) + 1;
-        return gemNumber;
-	}
-
-function appendNumber2Total(gemNum) {
-    
+    for (i = 1; i < 5; i++) {
+        let gemNumber = Math.floor(Math.random() * 12) + 1;
+        buttons.push(gemNumber)
+    }
 }
 
+function appendNumber2Total(gemNum) {}
 //this function is display the random generated number to appropriate html field
 function displayRandNumber(htmlTag) {
     randomNumber = generateRandNumber();
     htmlTag.html(randomNumber)
-    console.log('Random number supposed to display : ' + randomNumber);
 }
-
-
-
-
-
 
 function startGame() {
-    
-        console.log("you just clicked to start the game...");
-        // debugger;
-        generateGemRandNumber();
-                
-        displayRandNumber(randNumberHtml);
-    
+    console.log("you just clicked to start the game...");
+    // debugger;
+    generateGemRandNumber();
+    displayRandNumber(randNumberHtml);
 }
-
 
 function resetGame() {
     sum = 0;
+    $('#your-guess').html(sum);
     buttons.length = 0
-    
+    generateGemRandNumber();
+    displayRandNumber(randNumberHtml);
 }
